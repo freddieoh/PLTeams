dd46fcee3989aa9a63b6a62f5331f0320ac8db18
Fredrick Ohen <fredrickohen@gmail.com>
2018-03-26 13:09:46 -0500

    Created custom team cell

--- PLTeams/AppDelegate.swift
+++ PLTeams/AppDelegate.swift
@@ -20,7 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        let mainViewController = ViewController()
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        let mainViewController = ViewController(collectionViewLayout: flowLayout)
         window?.rootViewController = UINavigationController(rootViewController: mainViewController)
         
         
--- PLTeams/ViewController.swift
+++ PLTeams/ViewController.swift
@@ -8,14 +8,156 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellIdentifier = "TeamsCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView?.backgroundColor = .red
+        
+        collectionView?.register(TeamCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        
+        navigationItem.title = "Teams"
+        navigationController?.navigationBar.barTintColor = .purple
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.gray,
+                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 12)]
+        
+        
     }
-
-
-
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? TeamCell else {
+            fatalError("Dequed cell not an instance of TeamCell")
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width / 3) - 16, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsetsMake(8, 8, 8, 8)
+    }
+    
+    
 }
+
+
+class TeamCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        
+        setupCell()
+    }
+    
+    func setupCell() {
+        self.backgroundColor = .white
+        self.addSubview(teamImageView)
+        self.addSubview(teamLabel)
+        
+        teamImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 0, height: 50)
+        
+    }
+    
+    let teamImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .green
+        return imageView
+    }()
+    
+    let teamLabel: UILabel = {
+        let label = UILabel()
+        label.tintColor = .black
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .green
+        label.textAlignment = .center
+        label.text = "Team"
+        return label
+    }()
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?,
+                bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,
+                paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat,
+                paddingRight: CGFloat, width: CGFloat = 0, height: CGFloat = 0) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.topAnchor
+        }
+        return topAnchor
+    }
+    
+    var safeLeftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.leftAnchor
+        }
+        return leftAnchor
+    }
+    
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.bottomAnchor
+        }
+        return bottomAnchor
+    }
+    
+    var safeRightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.rightAnchor
+        }
+        return rightAnchor
+    }
+    
+}
+
 
