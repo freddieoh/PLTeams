9ebfb435862b568109aae588721d059e32da83e8
Fredrick Ohen <fredrickohen@gmail.com>
2018-03-26 12:11:04 -0500

    first commit

---
+++ PLTeams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PLTeams
+//
+//  Created by Fredrick Ohen on 3/26/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
